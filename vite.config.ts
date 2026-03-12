472a49ca307b9361dfaf622e4f0be8cdbc911552
ShawnSlawter <jasperzolphin@gmail.com>
2026-03-12 08:07:12 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/session-bookr/",
   server: {
     host: "::",
     port: 8080,
