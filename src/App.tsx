c482d2e64203e82dd41392f02ef14e70f6b3ac76
Shawn Slawter <Jasperzolphin@gmail.com>
2026-03-12 19:10:44 -0500

    Add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
@@ -19,6 +20,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </HashRouter>
+      <Analytics />
     </TooltipProvider>
   </QueryClientProvider>
 );
